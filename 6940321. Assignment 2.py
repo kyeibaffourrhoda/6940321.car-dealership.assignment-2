ccf1bd33b43f8d300f73b1f74615005d6ef9ce60
kyeibaffourrhoda <125649596+kyeibaffourrhoda@users.noreply.github.com>
2023-02-24 12:54:30 +0000

    Add files via upload

---
+++ 6940321. Assignment 2.py
@@ -0,0 +1,45 @@
+#different car brands and the prices
+carbrand = input("Please enter the car brand: ")
+if carbrand == "Porsche":
+    print('Yes and the price is GHC 9 million')
+elif carbrand == "Honda":
+    print('Yes and the price is GHC 7 million')
+elif carbrand == "Toyota":
+    print('Yes and the price is GHC 8 million')
+elif carbrand == "Volvo":
+    print('Yes and the price is GHC 6 million')
+elif carbrand == "Mazda":
+    print('Yes and the price is GHC 4 million')
+elif carbrand == "Tesla":
+    print('Yes and the price is GHC 10 million')
+elif carbrand == "Kia":
+    print('Yes and the price is GHC 3 million')
+elif carbrand == "Hyundai":
+    print('Yes and the price is GHC 5 million')
+elif carbrand == "Jaguar":
+    print('Yes and the price is GHC 4.5 million')
+elif carbrand == "Peugeot":
+    print('Yes and the price is GHC 7.5 million')
+elif carbrand == "Volkswagen":
+    print('Yes and the price is GHC 6.3 million')
+elif carbrand == "Ford":
+    print('Yes and the price is GHC 7.2 million')
+elif carbrand == "Benz":
+    print('Yes and the price isGHC 6.3 million')
+elif carbrand == "BMW":
+    print('Yes and the price is GHC 8.2 million')
+elif carbrand == "Nissan":
+    print('Yes and the price is GHC 6.7 million')
+elif carbrand == "Citroen":
+    print ('Yes and the price is GHC 7.9 million')
+elif carbrand == "Lexus":
+    print('Yes and the price is GHC 8.4 million')
+elif carbrand == "Fiat":
+    print('Yes and the price is GHC 5.1 million')
+elif carbrand == "Dacia":
+    print('Yes and the price is GHC 4.7 million')
+elif carbrand == "Audi":
+    print('Yes and the price is GHC 9.5 million')
+else:
+    print('No')
+#https://github.com/kyeibaffourrhoda/6940321.car-dealership.assignment-2.git
